29892e98d18f2e79dbb02101ae7388eba161835e
SimpleSuyash <suyashmaharjan@gmail.com>
2024-02-22 16:56:19 +1100

    seeds are created

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -1,5 +1,24 @@
 const {User} = require("../models");
 
 const userData = [
-    username: "simplesuyash"
+    {
+        username: "simplesuyash",
+        email: "mrsuyash@y7mail.com",
+        password: "@Suyash123"
+    },
+    {
+        username: "mahaan",
+        email: "mrmahaan@gmail.com",
+        password: "@Mahaan123"
+    },
+    {
+        username: "nivaan123",
+        email: "nivaan123@yahoo.com.au",
+        password: "@Nivaan123"
+    },
+    {
+        username: "sajana",
+        email: "ms.sajana@mail.com",
+        password: "@Sajana123"
+    }
 ];
