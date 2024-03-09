518c186539d46c0f448487c21e831af599718811
SimpleSuyash <suyashmaharjan@gmail.com>
2024-03-10 04:20:51 +1100

    routes refactored

--- controllers/index.js
+++ controllers/index.js
@@ -2,11 +2,14 @@ const router = require("express").Router();
 
 const homeRoutes = require("./home-routes");
 const apiRoutes = require("./api");
-const dashboardRoutes = require("./dashboard-routes");
+const dashboardRoutes = require("./dashboard");
+const loginRoutes = require('./login');
 
 router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
-router.use("/", dashboardRoutes);
+router.use('/dashboard', dashboardRoutes);
+router.use('/login', loginRoutes);
+
 
 
 module.exports = router;
